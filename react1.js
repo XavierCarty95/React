acb181cbe22c9bd95c1e02fce741e412d6bac760
Xavier Carty <xaviercarty.student@careerdevs.com>
2019-04-04 22:42:47 +0000

    What is react

---
+++ react1.js
@@ -0,0 +1,16 @@
+//REACT is a javascript library created by facebook 
+
+//currently used by netflix  && Instagram 
+
+// JS driven dynamic web apps 
+
+//Redux is an extra layer on top of react to help with state management of the app
+
+//React basics 
+// components , events , templates, propors & forms 
+// React Route 
+//routes , route parameters , redirects 
+
+//redux stores action & reduces 
+
+
