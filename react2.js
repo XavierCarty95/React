276620cf5111b1fe61812fcc8f24ca63d494db5b
Xavier Carty <xaviercarty.student@careerdevs.com>
2019-04-04 22:52:27 +0000

    how react works

---
+++ react2.js
@@ -0,0 +1,12 @@
+// React 
+
+// components components components
+
+
+// takes all of the components and creates a virtual dom and then renders DOM in actual DOM 
+
+// component look like Htm templates
+
+// they contain state (data or UI STATE )
+
+// THEY ALSO CONTAIN JAVASCRIPT FOR FUNCTIONALITY
