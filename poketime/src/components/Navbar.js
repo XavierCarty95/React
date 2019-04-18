50d5a06e79b9bb2bdc176ffe9dbf4952cec3b829
Xavier Carty <xaviercarty.student@careerdevs.com>
2019-04-18 23:41:25 +0000

    adding prop components

--- poketime/src/components/Navbar.js
+++ poketime/src/components/Navbar.js
@@ -1,26 +1,23 @@
-import React from 'react'
-import { Link, withRouter } from 'react-router-dom'
-
+import React from 'react';
+import { Link, NavLink, withRouter } from 'react-router-dom'
 
 const Navbar = (props) => {
-    console.log(props)
-    setTimeout(() => {
-        props.history('/about')
-    },2000)
-    return ( 
-        <nav className = "nav-wrapper red darken-3">
-        <div className = "container">
-        <div className="brand-logo"> Poke Times </div>
-        <ul className = "right">
-         <li><Link to="/">Home</Link></li>
-         <li><Link to="/about">About</Link></li>
-        <li><Link to="/contact">Contact</Link></li>
-         </ul>
-         </div>
-         </nav>
-         
-        
-        )
-}
 
+  setTimeout(() => {
+    props.history.push('/about');
+  }, 2000);
+
+  return (
+    <nav className="nav-wrapper red darken-3">
+      <div className="container">
+        <Link className="brand-logo" to="/">Poke' Times</Link>
+        <ul className="right">
+          <li><NavLink exact to="/">Home</NavLink></li>
+          <li><NavLink to='/about'>About</NavLink></li>
+          <li><NavLink to='/contact'>Contact</NavLink></li>
+        </ul>
+      </div>
+    </nav> 
+  )
+}
 export default withRouter(Navbar)
