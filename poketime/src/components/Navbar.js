6e861ff33baa68ba6a39379abb8f9aaa97dcd815
Xavier Carty <xaviercarty.student@careerdevs.com>
2019-04-18 18:18:38 +0000

    creating react component

--- poketime/src/components/Navbar.js
+++ poketime/src/components/Navbar.js
@@ -1,18 +1,21 @@
-import React from 'react';
+import React from 'react'
 
 
 const Navbar = () => {
-  return (
-    <nav className="nav-wrapper red darken-3">
-      <div className="container">
-        <a className="brand-logo">Poke' Times</a>
-        <ul className="right">
-          <li><a href="/">Home</a></li>
-          <li><a href='/about'>About</a></li>
-          <li><a href='/contact'>Contact</a></li>
-        </ul>
-      </div>
-    </nav> 
-  )
+    return ( 
+        <nav className = "nav-wrapper red darken-3">
+        <div className = "container">
+        <div className="brand-logo"> Poke Times </div>
+        <ul className = "right">
+         <li><a href ="/">Home</a></li>
+         <li><a href ="/about">About</a></li>
+         <li><a href ="/contact">Contact</a></li>
+         </ul>
+         </div>
+         </nav>
+         
+        
+        )
 }
+
 export default Navbar
