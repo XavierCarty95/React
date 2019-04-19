6b9bd043b60dfb0e277e53d92e8a81ff36e5f93a
Xavier Carty <xaviercarty.student@careerdevs.com>
2019-04-19 15:06:19 +0000

    using axios to get data from url

--- poketime/src/components/Home.js
+++ poketime/src/components/Home.js
@@ -1,13 +1,52 @@
-import React from 'react'
+import React, { Component } from 'react'
+import axios from 'axios'
 
-const Home = () => {
-  return (
-    <div>
-      <div className="container">
+class Home extends Component {
+
+    state = {
+        posts: []
+    }
+
+    componentDidMount() {
+        axios.get('https://jsonplaceholder.typicode.com/posts')
+            .then(res => {
+                console.log(res)
+                this.setState({
+                    posts: res.data.slice(0, 10)
+                })
+            })
+
+    }
+    render() {
+
+        const { post } = this.state
+        const postList = post.length ? (post.map(post => {
+            return (<div className = "post-card" key={post.id}>
+                    <div className = "card-content">
+                    <span className = "card-title">{post.title}</span>
+                    <p> {post.body}</p>
+                </div>
+            </div>
+            
+            )
+        })
+        
+        ) : (
+
+
+            <div className = "center"> No posts yet </div>
+            )
+        return (
+
+     <div className="container">
         <h4 className="center">Home</h4>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae repudiandae repellat illo magni eligendi cupiditate voluptates eius nam voluptate. Incidunt nihil ullam quae quia officia quaerat, deserunt eligendi explicabo totam?</p>
-      </div>
+      
+        {postList}
     </div>
-  )
+        )
+
+
+
+    }
 }
 export default Home
